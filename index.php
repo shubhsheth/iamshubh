2d43734c722b41b7df6c39198bef6ee472154860
iamshubh <iamshubh@iamshubh.com>
2020-04-02 06:40:55 +0000

    Initialized

---
+++ index.php
@@ -0,0 +1,287 @@
+<html>
+    <head>
+        <?php include_once('includes/head.php'); ?>
+    </head>
+    <body>
+        <main>
+            <?php include_once('includes/header.php'); ?>
+            
+            <!-----HERO - Section 01----->
+            <section class="hero">
+                <div class="carousel_outer">
+                    <div class="carousel">
+                        <!--Carousel Slide 01-->
+                        <figure class="carousel_slide carousel_slide-1">
+                            <div class="slide_content">
+                                <div class="svg-wrapper">
+                                  <svg height="60" width="320" xmlns="https://www.w3.org/2000/svg">
+                                    <rect class="shape" height="60" width="320" />
+                                  </svg>
+                                   <h1 class="title">iamshubh</h1>
+                                </div>
+                            </div>
+                        </figure>
+                        <!--Carousel Slide 02-->
+                        <figure class="carousel_slide carousel_slide-2">
+                            <div class="slide_content">
+                                <div id="particles"></div>
+                                
+                                <div class="connectCardCont push-xl-2 push-md-1">
+                                    <img src="img/business-card.jpg" class="connect_cadrd">
+                                </div>
+                                
+                                <div class="col-md-6 col-12 push-md-6 social-icons-cont">
+                                    <ul class="social-icons-list">
+                                        <li>
+                                            <span class="fb">
+                                                <img src="img/icon-fb.png">
+                                            </span>
+                                            <a href="#">Facebook</a>
+                                        </li>
+                                        <li>
+                                            <span class="ig">
+                                                <img src="img/icon-ig.png">
+                                            </span>
+                                            <a href="#">Instagram</a>
+                                        </li>
+                                        <li>
+                                            <span class="in">
+                                                <img src="img/icon-in.png">
+                                            </span>
+                                            <a href="#">LinkedIn</a>
+                                        </li>
+                                        <li>
+                                            <span class="tw">
+                                                <img src="img/icon-tw.png">
+                                            </span>
+                                            <a href="#">Twitter</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </figure>
+                        <!--Carousel Slide 03-->
+                        <figure class="carousel_slide carousel_slide-3">
+                            <div class="slide_content">
+                                <span>
+                                    <h3>Wait for the</h3><h1>Best</h1>
+                                </span>
+                            </div>
+                        </figure>
+                        <!--Carousel Slide 04-->
+                        <figure class="carousel_slide carousel_slide-4">
+                            <div class="slide_content">
+                                <span>
+                                    <h3>The Best is</h3><h1>Coming</h1>
+                                </span>
+                            </div>
+                        </figure>
+                        
+                        <!--Placeholder Slides for Transition Effects-->
+                        <figure class="carousel_slide carousel_slide-5">
+                            <img src="img/new%20york.jpg">
+                        </figure>
+                        <figure class="carousel_slide carousel_slide-6"></figure>
+                    </div>
+                </div>
+                <div class="carousel_controls">
+                    <div class="container">
+                        <div class="row">
+                            <!--Carousel Button 01-->
+                            <div class="carousel_control col-lg-2 col-3 carousel_control-active">
+                                <a href="#" class="btn-carousel_control"></a>
+                                <h4 class="carousel_control_button_name">Intro</h4>
+                            </div>
+                            <!--Carousel Button 02-->
+                            <div class="carousel_control col-lg-2 col-3">
+                                <a href="#" class="btn-carousel_control"></a>
+                                <h4 class="carousel_control_button_name">Connect</h4>
+                            </div>
+                            <!--Carousel Button 03-->
+                            <div class="carousel_control col-lg-2 col-3">
+                                <a href="#" class="btn-carousel_control"></a>
+                                <h4 class="carousel_control_button_name">Game</h4>
+                            </div>
+                            <!--Carousel Button 04-->
+                            <div class="carousel_control col-lg-2 col-3">
+                                <a href="#" class="btn-carousel_control"></a>
+                                <h4 class="carousel_control_button_name">Reff.</h4>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="nojs">
+                    <div>
+                        <h1>iamshubh</h1>
+                    </div>
+                </div>
+            </section><!--hero end-->
+            
+            <!-----ABOUT - Section 02----->
+            <section class="home-about">
+                <div class="parallax_about parallax_about-two">
+                    <img src="img/about.png" class="img_about">
+                </div>
+                <div class="parallax_about">
+                    <img src="img/about.png" class="img_about">
+                </div>
+                <div class="container">
+                    <div class="row">   
+                        <div class="col-md-7 col-lg-6  abt_img ">
+                            <span class="abt_img_portrait"></span>
+<!--                            <img src="img/portrait.png" class="abt_img_portrait">-->
+                        </div>
+                        <div class="col-md-5 col-lg-6 abt_info">
+                            <div class="wrapper">
+                                <h1 class="abt_info_name">Shubh Sheth</h1>
+                                <h2 class="abt_info_job">//Web Developer</h2>
+                                <p class="abt_info_summary">
+                                    Hello, my name is Shubh. I am a student in
+                                    Computer Science at the University of Houston. I
+                                    am also a Web Developer at RegexSEO in Houston.
+                                    <br><br>
+                                    I had a passion for computers since I was very young.
+                                    But, when I was 14 I stumbled upon programming. I have
+                                    kept learning more and more interesting stuff till than.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section><!--about end-->
+            
+            
+            <!--Porfolio-->
+            <section class="index-portfolio">
+                <div class="container">
+                    <img src="img/render.png" class="portfolio-header">
+                    <div class="row">
+                        <div class="col-lg-7 col-12">
+                            <div class="row">
+                                <div class="desktop-frame">
+                                    <img src="img/imac.png">
+                                    <div class="desktop-carousel">
+                                        <div class="carousel-container">
+                                            <div class="item">
+                                                <img src="img/ss-desktop-01.jpg">      
+                                            </div>
+                                            <div class="item">
+                                                <img src="img/ss-desktop-02.jpg">      
+                                            </div>
+                                            <div class="item">
+                                                <img src="img/ss-desktop-03.jpg">      
+                                            </div>
+                                            <div class="item">
+                                                <img src="img/ss-desktop-04.jpg">      
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="tablet-frame">
+                                    <img src="img/ipad.png">
+                                    <div class="tablet-carousel">
+                                        <div class="carousel-container">
+                                            <div class="item">
+                                                <img src="img/ss-tablet-01.jpg">      
+                                            </div>
+                                            <div class="item">
+                                                <img src="img/ss-tablet-02.jpg">      
+                                            </div>
+                                            <div class="item">
+                                                <img src="img/ss-tablet-03.jpg">      
+                                            </div>
+                                            <div class="item">
+                                                <img src="img/ss-tablet-04.jpg">      
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="mobile-frame">
+                                    <img src="img/iphone.png">
+                                    <div class="mobile-carousel">
+                                        <div class="carousel-container">
+                                            <div class="item">
+                                                <img src="img/ss-mobile-01.jpg">      
+                                            </div>
+                                            <div class="item">
+                                                <img src="img/ss-mobile-02.jpg">      
+                                            </div>
+                                            <div class="item">
+                                                <img src="img/ss-mobile-03.jpg">      
+                                            </div>
+                                            <div class="item">
+                                                <img src="img/ss-mobile-04.jpg">      
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <a class="btn-up portfolio-button btn-lighten"><img src="img/btn-up.png" onclick="down(0)"></a>
+                            <a class="btn-down portfolio-button"><img src="img/btn-down.png" onclick="down(1)"></a>
+                        </div>
+                        <div class="col-lg-5 col-12">
+                            <div class="text-carousel" style="height:200px">
+                                <div class="carousel-container">
+                                    <div class="item">
+                                        <h1>SD Card Holder</h1>
+                                        <p>
+                                            orem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut 
+                                        </p>
+                                        <ul>
+                                            <li>elit, sed do eiusmod tempor
+                                        incididunt ut la</li>
+                                            <li>eiusmod tempor
+                                        incididunt ut la</li>
+                                        </ul>
+                                    </div>
+                                    <div class="item">
+                                        <h1>PBX Pad</h1>
+                                        <p>
+                                            orem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut 
+                                        </p>
+                                        <ul>
+                                            <li>elit, sed do eiusmod tempor
+                                        incididunt ut la</li>
+                                            <li>eiusmod tempor
+                                        incididunt ut la</li>
+                                        </ul>
+                                    </div>
+                                    <div class="item text1">
+                                        <h1>Khan Driving Academy</h1>
+                                        <p>
+                                            orem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut 
+                                        </p>
+                                        <ul>
+                                            <li>elit, sed do eiusmod tempor
+                                        incididunt ut la</li>
+                                            <li>eiusmod tempor
+                                        incididunt ut la</li>
+                                        </ul>
+                                    </div>
+                                    <div class="item">
+                                        <h1>Mr. Rockztar</h1>
+                                        <p>
+                                            orem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut 
+                                        </p>
+                                        <ul>
+                                            <li>elit, sed do eiusmod tempor
+                                        incididunt ut la</li>
+                                            <li>eiusmod tempor
+                                        incididunt ut la</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            
+            <?php include_once('includes/blog-intro.php'); ?>
+            <?php include_once('includes/cta.php'); ?>
+            
+        </main>
+        <!-----FOOTER----->
+        <?php include_once('includes/footer.php'); ?>
+    </body>
+</html>
