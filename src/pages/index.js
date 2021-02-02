a468e59769c8df980e732b0f74b8eab0045f82f1
Shubh Sheth <shubhsheth8@gmail.com>
2021-02-01 22:52:50 -0600

    Updated Home Page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,3 @@
-import "bootstrap/dist/css/bootstrap-grid.min.css"
 import React from "react"
 import Layout from "../layouts/layout"
 import SEO from "../components/seo"
@@ -6,14 +5,12 @@ import SEO from "../components/seo"
 
 import Hero from "../components/hero/hero"
 import About from "../components/about/about"
-import Banner from "../components/banner/banner"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <Hero />
     <About />
-    <Banner />
   </Layout>
 )
 
