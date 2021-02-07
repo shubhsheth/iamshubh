5e3544e102c499907b3e8ffa8d30ad6db4f40106
Shubh Sheth <shubhsheth8@gmail.com>
2021-02-06 23:34:27 -0600

    Removed old header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -23,24 +23,6 @@ const Header = ({ siteTitle }) => {
 
     return (
         <>
-            <header className="header">
-                <div className="header-inner">
-                    <Link to="/"><h1>{siteTitle}</h1></Link>
-                    <a href={void(0)}
-                        className={isToggled ? 'active icon' : 'icon'}
-                        onClick={navClickHandler} >
-                        <div className="hamburger"></div>
-                    </a>
-                </div>
-            </header>
-            <nav className={!isToggled ? 'close' : ''}>
-                <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/blog/">Blog</Link></li>
-                    <li><Link to="/#home-contact">Portfolio</Link></li>
-                    <li><Link to="/#home-contact">Contact</Link></li>
-                </ul>
-            </nav>
         </>
     )
 }
